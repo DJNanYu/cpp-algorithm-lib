4c12166b068584f2b64feeecd49ec877111e9aed
DJNanYu <2986945687@qq.com>
2022-02-15 10:00:00 -0500

    feat: initial draft of cpp-algorithm-lib

---
+++ README.md
@@ -0,0 +1,5 @@
+# cpp-algorithm-lib
+
+Custom String Obfuscation Library. 自定义字符串混淆库
+
+Initial development of cpp-algorithm-lib.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Draft version for cpp-algorithm-lib
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// 模拟用于混淆的自定义字符串加解密算法
+// 逆向中常见的保护手段
+class ...
